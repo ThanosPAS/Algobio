33b35185319fe119f09438ea6bf6dc0a69c03a17
Hannah-Marie Martiny <hanmar@food.dtu.dk>
2020-06-17 11:59:14 +0200

    preprocessing.py now with argparse instead of absolute paths

--- preprocessing.py
+++ preprocessing.py
@@ -1,35 +1,69 @@
-
+#!/bin/python3
 
 import numpy as np
 import pandas as pd
+import argparse
+import os
 
+def parse_args():
+    parser = argparse.ArgumentParser("Concat data")
 
+    parser.add_argument(
+        '-d', '--data-dir',
+        type=str,
+        help='Path to data dir with files to be concatenated',
+        required=True,
+        dest='data_dir'
+    )
 
-MAX_PEP_SEQ_LEN = 9
+    parser.add_argument(
+        '-o', '--output-file',
+        type=str,
+        required=True,
+        dest='output_file',
+        help='Output file concatenated data is stored in'
+    )
+    
+    return parser.parse_args()
 
-files = ["c001","c002","c003","c004","f000","f001","f002","f003","f004"]
-
-def load_peptide_target(filename):
+def load_peptide_target(filename, MAX_PEP_SEQ_LEN=9):
     """
     Read amino acid sequence of peptides and
     corresponding log transformed IC50 binding values from text file.
     """
-    df = pd.read_csv(filename, sep='\s+', usecols=[0,1], names=['peptide','target'])
-    return df[df.peptide.apply(len) <= MAX_PEP_SEQ_LEN]
+    # load
+    df = pd.read_csv(filename, sep=' ', usecols=[0,1], names=['peptide','target'])
 
+    # filter by peptide length
+    df = df.loc[df.peptide.str.len() <= MAX_PEP_SEQ_LEN]
+    return df
 
+if __name__ == "__main__":
+    args = parse_args()
 
-train_raw = pd.read_csv("C:/Users/white/Google Drive/Master's/June 2020/Algorithms in Bioinformatics/Algobio/data/A0201/c000", sep='\s+', usecols=[0,1], names=['peptide','target'])
-for batch in files:
-    train_data = "C:/Users/white/Google Drive/Master's/June 2020/Algorithms in Bioinformatics/Algobio/data/A0201/"
-    train_data = train_data + batch
-    pre_train = load_peptide_target(train_data)
-    train_raw = pd.concat([train_raw,pre_train], ignore_index = True)
-    pre_train.iloc[0:0]
+    files = [x for x in os.listdir(args.data_dir) if not x.endswith('.dat')]
 
-train_raw.sort_values(by=['target'], inplace=True, ascending=False)
-train_raw.drop_duplicates(inplace = True)
-train_raw.index = range(train_raw.shape[0])
+    # loop through files in datadir and load data
+    outs = [] 
+    for f in files:
+        f = os.path.join(args.data_dir, f)
+        d = load_peptide_target(f)
+        outs.append(d)
+    
+    # concat to one df
+    df = pd.concat(outs, axis=0)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    # write to output file
+    df.to_csv(args.output_file, index=None)
+
+    print("CSV file saved:", args.output_file)
+    print("Shape:", df.shape)
+    print(df.head())
+
+# python preprocessing.py -d data/A0201/ -o data/A0201.csv
 
 
     
